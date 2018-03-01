f71ed0cc266968f2f8b07253956dfc1616942d03
fernandasartori <feernanda-cristina@hotmail.com>
2018-03-01 20:30:17 -0300

    inicio

---
+++ Controllers/ProdutoController.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductManager.Models;
+
+namespace ProductManager.Controllers
+{
+    public class ProdutoController : Controller
+    {
+
+        static List<Produto> listaDeProdutos = new List<Produto>
+        {
+            new Produto{Id = 1, Nome= "Tomato Soup", Categoria = "Groceries", Preco = 1},
+            new Produto{Id = 2, Nome= "Ioio", Categoria = "Toys", Preco = 3.78M},
+            new Produto{Id = 3, Nome= "Hammer", Categoria = "Hardware", Preco = 16.99M}
+
+
+        };
+
+
+
+
+        // GET: Produto
+        public ActionResult Index()
+        {
+            return View(listaDeProdutos);
+        }
+
+        // GET: Produto/Details/5
+        public ActionResult Details(int id)
+        {
+            var produto = listaDeProdutos.FirstOrDefault(x => x.Id == id);
+            return View(produto);
+        }
+
+        // GET: Produto/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Produto/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Produto produto)
+        {
+            try
+            {
+                listaDeProdutos.Add(produto);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Produto/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var produto = listaDeProdutos.FirstOrDefault(m => m.Id == id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+                   
+            return View(produto);
+        }
+
+        // POST: Produto/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, [Bind("Id, Nome, Categoria, Preco")] Produto produto)
+        {
+            try
+            {
+                if (id != produto.Id)
+                {
+                    return NotFound();
+                }
+
+                if (ModelState.IsValid)
+                {
+                    var produtoTemp = listaDeProdutos.FirstOrDefault(c => c.Id == id);
+
+                    if (produtoTemp != null)
+                    {
+                        produtoTemp.Categoria = produto.Categoria;
+                        produtoTemp.Nome = produto.Nome;
+                        produtoTemp.Preco = produto.Preco;
+
+                    }
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            catch
+            {
+                return View(produto);
+            }
+        }
+
+        // GET: Produto/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var produto = listaDeProdutos.FirstOrDefault(m => m.Id == id);
+            
+            if (produto == null)
+            {
+                return NotFound();
+
+            }
+            return View(produto);
+
+
+        }
+
+        // POST: Produto/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirm(int id )
+        {
+            try
+            {
+                var produto = listaDeProdutos.FirstOrDefault(m => m.Id == id);
+                listaDeProdutos.Remove(produto);
+
+                return RedirectToAction(nameof(Index));
+                
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductManager.Models;
+
+namespace ProductManager.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        static List<Usuario> listaDeUsuario = new List<Usuario>
+        {
+            new Usuario{Id = 1, Nome= "Alfredo", Cpf = "42542542542", Email = "alfredohtinha@gmail.com"},
+            new Usuario{Id = 2, Nome= "Camila", Cpf = "77788899912", Email = "camilahtona@gmail.com"},
+            new Usuario{Id = 3, Nome= "Fernanda", Cpf = "717171717171", Email = "fernandarainha@gmail.com"}
+
+        };
+
+
+
+
+
+
+
+        public ActionResult Index()
+        {
+            return View(listaDeUsuario);
+        }
+
+        // GET: Usuario/Details/5
+        public ActionResult Details(int id)
+        {
+            var usuario = listaDeUsuario.FirstOrDefault(u => u.Id == id);
+            return View(usuario);
+        }
+
+        // GET: Usuario/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Usuario/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Usuario usuario)
+        {
+            try
+            {
+                listaDeUsuario.Add(usuario);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Usuario/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuario = listaDeUsuario.FirstOrDefault(u => u.Id == id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuario);
+
+        }
+
+        // POST: Usuario/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, [Bind("Id, Nome, Cpf, Email")] Usuario usuario)
+        {
+            try
+            {
+                if (id != usuario.Id)
+                {
+                    return NotFound();
+                }
+
+                if (ModelState.IsValid)
+                {
+                    var usuarioTemp = listaDeUsuario.FirstOrDefault(u => u.Id == id);
+
+                    if (usuarioTemp != null)
+                    {
+                        usuarioTemp.Nome = usuario.Nome;
+                        usuarioTemp.Cpf = usuario.Cpf;
+                        usuarioTemp.Email = usuario.Email;
+
+                    }
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            catch
+            {
+                return View(usuario);
+            }
+        }
+
+        // GET: Usuario/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var usuario = listaDeUsuario.FirstOrDefault(u => u.Id == id);
+
+            if (usuario == null)
+            {
+                return NotFound();
+
+            }
+            return View(usuario);
+
+
+        }
+
+        // POST: Produto/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirm(int id)
+        {
+            try
+            {
+                var usuario = listaDeUsuario.FirstOrDefault(u => u.Id == id);
+                listaDeUsuario.Remove(usuario);
+
+                return RedirectToAction(nameof(Index));
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
